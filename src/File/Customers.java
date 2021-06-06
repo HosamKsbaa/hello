7174fb586b31751002eb29a4942cc44397a9e1ed
amrAhmed-dev <59887686+amrAhmed-dev@users.noreply.github.com>
2021-06-06 02:56:40 +0200

    change In database and customer

--- src/File/Customers.java
+++ src/File/Customers.java
@@ -4,14 +4,23 @@ import utility.Io;
 
 public class Customers {
     
-    public  void reserveTicket(){
+    public  void reserveTicket(String text){
+        FileDataBase f = new FileDataBase();
+        f.fileName ="check.txt";
+        if(f.check(text)){
+            f.fileName="text.txt";
+            Io.print(f.readData(text));
+        }
+        else
+        Io.print("your ticket ha expired");
         
         //Io.print("<newTicket code>");
     }
+
     public   void cancelReservation(){
-       // String code =Io.cin("add your code");
-         
+
     }
+
     public void notifyStaffwithcancelCustomer(int currentCustomer){
         String x=String.format("A string %s ", currentCustomer+1 ," get ready ");
         Io.print(x);
