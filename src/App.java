317817588cf51e70d9f39dbe0aa73f9c529c35fb
amrAhmed-dev <59887686+amrAhmed-dev@users.noreply.github.com>
2021-06-06 09:18:19 +0200

    hosam

--- src/App.java
+++ src/App.java
@@ -3,6 +3,7 @@ import File.QueueManger;
 public class App {
     
     public static void main(String[] args) throws Exception {
+        
         QueueManger queueManger=new QueueManger();
         queueManger.StrartTheAPp();
     }
