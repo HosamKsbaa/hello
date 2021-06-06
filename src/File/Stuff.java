09e399ae0e388f8eb8e24231b3e69c7aed8c3958
amrAhmed-dev <59887686+amrAhmed-dev@users.noreply.github.com>
2021-06-06 06:58:36 +0200

    amdsa

--- src/File/Stuff.java
+++ src/File/Stuff.java
@@ -1,15 +1,44 @@
 package File;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+
 
 //import utility.Io;
 
 public class Stuff {
    
-     void processAClient(){
-        FileDataBase f =new FileDataBase();
-        f.fileName ="text.txt";
-        f.fileName ="done.txt";
-        //if <done.txt>.length == 0 
-        // done.txt.write <text.txt>.first
+     String processAClient(){
+        String currentGuy="";
+
+        FileDataBase doneF = new FileDataBase();
+        doneF.fileName="done.txt";
+        
+        if(!doneF.exists()){
+            Io.print("File f line f");
+                doneF.createNewFile();
+                System.out.println("File created: " + myObj.getName());            
+            currentGuy=FileDataBase.readFromLast("text.txt", 1);
+
+        }else if(doneF.length()<1){
+            Io.print("File t line f");
+            currentGuy=FileDataBase.readFromLast("text.txt", 1);
+        } 
+        else{
+            Io.print("File t line T");
+            String prevuiosGuy=FileDataBase.readFromLast("done.txt", 0);
+            FileDataBase f = new FileDataBase();
+            Io.print("prevuiosGuy "+prevuiosGuy);
+           //================================
+            f.fileName="text.txt";
+            currentGuy =f.readData(prevuiosGuy);
+            Io.print("currentGuy "+currentGuy);
+
+            
+        }        
+        doneF.write( currentGuy);
+        return currentGuy;
     }
 
     public void notifyStaffWithNewCustomer(){
--- src/File/database.java
+++ src/File/database.java
@@ -10,7 +10,96 @@ class FileDataBase implements InterfaceDB{
 
 String fileName;
 
+public static String  readFromLast(String fileName,int t){  
+    File file = new File(fileName);
+    int lines = 0;
+    StringBuilder builder = new StringBuilder();
+    RandomAccessFile randomAccessFile = null;
+    try {
+      randomAccessFile = new RandomAccessFile(file, "r");
+      //Io.print(Long.toString(file.length() - 1));
 
+      if(t==1){
+        long fileLength = 0;
+        randomAccessFile.seek(fileLength);
+        char c;
+        long pointer = fileLength;
+        randomAccessFile.seek(pointer);
+        c = (char)randomAccessFile.read(); 
+
+        while (c != '\n'){
+
+            builder.append(c);
+            pointer++;
+
+            randomAccessFile.seek(pointer);
+            c = (char)randomAccessFile.read(); 
+
+            
+        }
+        
+      }
+      else{
+        long fileLength = file.length() - 1;
+
+        randomAccessFile.seek(fileLength);
+        for(long pointer = fileLength; pointer >= 0; pointer--){
+          randomAccessFile.seek(pointer);
+          char c;
+          // read from the last one char at the time
+          c = (char)randomAccessFile.read(); 
+          // break when end of the line
+          if(c == '\n'){
+            break;
+          }
+          builder.append(c);
+        }
+      }
+      //https://www.netjstech.com/2016/02/how-to-read-file-from-last-line-in-java.html
+     //long fileLength = 100;
+      // Set the pointer at the last of the file
+      
+      // Since line is read from the last so it 
+      // is in reverse so use reverse method to make it right
+      builder.reverse();
+      randomAccessFile.close();
+
+      if(t==1){
+       String x = builder.toString();
+       String y =new StringBuilder(x).reverse().toString();
+        return y;
+        // System.out.println("Line - " + y);
+
+        }
+        else{
+            return builder.toString();
+            // System.out.println("Line - " + builder.toString());
+
+        }
+                        
+    } catch (FileNotFoundException e) {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+      return "dg 7amobika";
+    }
+    catch (IOException e) {
+      // TODO Auto-generated catch block
+      
+      e.printStackTrace();
+      return "dg 7amobika";
+    }
+    finally{
+      if(randomAccessFile != null){
+        try {
+          randomAccessFile.close();
+        } catch (IOException e) {
+          // TODO Auto-generated catch block
+          e.printStackTrace();
+        }
+      }
+     // 
+}
+  }
 
  
 @Override
@@ -18,8 +107,7 @@ public void write(String text){
 
    try {
       File myObj = new File(this.fileName);
-
-      
+    
         if (!myObj.exists()){
         myObj.createNewFile();
         System.out.println("File created: " + myObj.getName());
@@ -43,9 +131,7 @@ public void write(String text){
         File myObj = new File(this.fileName);
         String data ="";
         if (myObj.exists()) {
-            Scanner s =search(text);
-            data = s.nextLine();
-            s.close();
+            data =search(text);
             return data;
         }
         else
@@ -53,27 +139,33 @@ public void write(String text){
             return "";
         }
         
+     
+     
+     
         @Override
-        public Scanner search(String text){
-            Scanner myReader;
+        public String search(String text){
+            String data="";            
+            Scanner  myReader;
             try{
-             myReader= new Scanner(new File(this.fileName));
-            while (myReader.hasNextLine() && myReader.nextLine() != text) {
-                myReader.nextLine();
+            myReader= new Scanner(new File(this.fileName));
         }
-    }
-    
         catch(Exception e){
             System.out.println("Error file used inside search function doesn't exist");
-             myReader = new Scanner("");
+             return "nothing there is error";
         }
-        return myReader;
+        while (myReader.hasNextLine() && myReader.) {
+            data=myReader.nextLine();
+        }
+        return data;
     }
     
+
+
+
+
     @Override
-    public boolean check(String text){
-        Scanner s =search(text);
-         String t=s.nextLine();
+    public boolean check(String text){        
+         String t = search(text);
         if(t =="finish")
             return false;
         else
