251466af6a51e2b545402ae20b4474233d276b9e
amrAhmed-dev <59887686+amrAhmed-dev@users.noreply.github.com>
2021-06-06 02:58:36 +0200

    change in database & custoemr

--- src/File/InterfaceDB.java
+++ src/File/InterfaceDB.java
@@ -1,9 +1,9 @@
-import java.io.File;
+package File;
 import java.util.Scanner;
 
 interface InterfaceDB{
     public abstract void write(String text);
     public abstract String readData(String text);
-    public abstract Scanner search(String text ,File myObj);
-    public abstract void delete(String text);
+    public abstract Scanner search(String text);
+    public abstract boolean check(String text);
 }
